df58bbf19179c6992b6dd34b1cd1a494cc3470fc
AlexZeller <alexander.zeller@t-online.de>
2021-03-23 20:55:47 +0100

    Uncomment Server

--- atmo-server/server.js
+++ atmo-server/server.js
@@ -5,13 +5,13 @@ const https = require('https')
 const fs = require('fs')
 const port = 443
 
-//app.use(express.static(path.join(__dirname, '../atmo-ui/dist')));
+app.use(express.static(path.join(__dirname, '../atmo-ui/dist')));
 
-//const httpsOptions = {
-//    key: fs.readFileSync('certificates/atmo.local.key'),
-//    cert: fs.readFileSync('certificates/atmo.local.crt')
-//}
-//const server = https.createServer(httpsOptions, app)
-//    .listen(port, () => {
-//        console.log('server running at ' + port)
-//    })
+const httpsOptions = {
+    key: fs.readFileSync('certificates/atmo.local.key'),
+    cert: fs.readFileSync('certificates/atmo.local.crt')
+}
+const server = https.createServer(httpsOptions, app)
+    .listen(port, () => {
+        console.log('server running at ' + port)
+    })
