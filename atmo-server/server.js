998c9c2fd778e97f47e4c8fecd7068d837d2ddc2
AlexZeller <alexander.zeller@t-online.de>
2021-04-11 08:47:29 +0200

    Remove PWA support

--- atmo-server/server.js
+++ atmo-server/server.js
@@ -1,17 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path');
-const https = require('https')
-const fs = require('fs')
-const port = 443
+const port = 80
 
 app.use(express.static(path.join(__dirname, '../atmo-ui/dist')));
 
-const httpsOptions = {
-    key: fs.readFileSync(path.join(__dirname, 'certificates/atmo.local.key')),
-    cert: fs.readFileSync(path.join(__dirname, 'certificates/atmo.local.crt'))
-}
-const server = https.createServer(httpsOptions, app)
-    .listen(port, () => {
-        console.log('server running at ' + port)
-    })
+app.listen(port, () => {
+    console.log('server running at ' + port)
+})
